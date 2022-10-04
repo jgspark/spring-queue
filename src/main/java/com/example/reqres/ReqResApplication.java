adca081f4ef1ad0d489240d0f75bacfd9498c3e9
newbalancer <pnw123456@icloud.com>
2022-10-04 19:35:12 +0900

    feat: rename project

--- src/main/java/com/example/reqres/ReqResApplication.java
+++ src/main/java/com/example/reqres/ReqResApplication.java
@@ -1,13 +1,13 @@
-package com.example.stock;
+package com.example.reqres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockApiApplication {
+public class ReqResApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockApiApplication.class, args);
+        SpringApplication.run(ReqResApplication.class, args);
     }
 
 }
