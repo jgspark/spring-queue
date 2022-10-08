73af3ae731524391ed6442c27c9939a34c32c8c3
newbalancer <pnw123456@icloud.com>
2022-10-08 11:33:53 +0900

    refactor: class to recode changed

--- src/main/java/com/example/messagingrabbitmq/sample/CustomMessage.java
+++ src/main/java/com/example/messagingrabbitmq/sample/CustomMessage.java
@@ -1,17 +1,4 @@
 package com.example.messagingrabbitmq.sample;
 
-import lombok.*;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-public class CustomMessage {
-    private String text;
-    private int index;
-
-    public CustomMessage(String text, int index) {
-        this.text = text;
-        this.index = index;
-    }
+public record CustomMessage(String text, int index) {
 }
