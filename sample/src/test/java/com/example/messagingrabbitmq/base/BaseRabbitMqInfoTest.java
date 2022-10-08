fb8e6f363f5010b615fc74ea1f66af9703e14ad5
newbalancer <pnw123456@icloud.com>
2022-10-08 19:04:18 +0900

    feat: bean add to RabbitListenerEndpointRegistry

--- sample/src/test/java/com/example/messagingrabbitmq/base/BaseRabbitMqInfoTest.java
+++ sample/src/test/java/com/example/messagingrabbitmq/base/BaseRabbitMqInfoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,20 +19,20 @@ public class BaseRabbitMqInfoTest {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-//    @Autowired
+    //    @Autowired
 //    private RabbitAdmin admin;
 //
-//    @Autowired
-//    private RabbitListenerEndpointRegistry registry;
+    @Autowired
+    private RabbitListenerEndpointRegistry registry;
 
 
     @Test
     @DisplayName("연결을 위한 테스트 케이스")
     void testConnection() {
 
-        CustomMessage message = new CustomMessage("test message", 0);
-
-        rabbitTemplate.convertAndSend(RabbitMQTestConfiguration.topicExchangeName, RabbitMQTestConfiguration.routingKey, message);
+//        CustomMessage message = new CustomMessage("test message", 0);
+//
+//        rabbitTemplate.convertAndSend(RabbitMQTestConfiguration.topicExchangeName, RabbitMQTestConfiguration.routingKey, message);
 
     }
 }
--- sample/src/test/java/com/example/messagingrabbitmq/config/RabbitMQTestConfiguration.java
+++ sample/src/test/java/com/example/messagingrabbitmq/config/RabbitMQTestConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.amqp.rabbit.test.RabbitListenerTest;
 import org.springframework.amqp.rabbit.test.context.SpringRabbitTest;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -61,5 +62,10 @@ public class RabbitMQTestConfiguration {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    public RabbitListenerEndpointRegistry registry() {
+        return new RabbitListenerEndpointRegistry();
     }
 }
