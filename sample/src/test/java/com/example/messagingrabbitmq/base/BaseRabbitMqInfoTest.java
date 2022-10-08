287b5b68a4c91760e03726de82a1394a9323f179
newbalancer <pnw123456@icloud.com>
2022-10-08 18:50:28 +0900

    test: rabbitmq test case add this case is fail ..

---
+++ sample/src/test/java/com/example/messagingrabbitmq/base/BaseRabbitMqInfoTest.java
@@ -0,0 +1,37 @@
+package com.example.messagingrabbitmq.base;
+
+import com.example.messagingrabbitmq.config.RabbitMQTestConfiguration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        RabbitMQTestConfiguration.class
+})
+public class BaseRabbitMqInfoTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+//    @Autowired
+//    private RabbitAdmin admin;
+//
+//    @Autowired
+//    private RabbitListenerEndpointRegistry registry;
+
+
+    @Test
+    @DisplayName("연결을 위한 테스트 케이스")
+    void testConnection() {
+
+        CustomMessage message = new CustomMessage("test message", 0);
+
+        rabbitTemplate.convertAndSend(RabbitMQTestConfiguration.topicExchangeName, RabbitMQTestConfiguration.routingKey, message);
+
+    }
+}
