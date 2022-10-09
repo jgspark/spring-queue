7d6a4c2babfe1bdbcf8cd846cc58518d8c1869f2
newbalancer <pnw123456@icloud.com>
2022-10-09 20:11:22 +0900

    test: 유저 아이디별 조회 테스트 케이스

--- email-service/src/test/java/com/example/messagingrabbitmq/user/UserRepositoryTest.java
+++ email-service/src/test/java/com/example/messagingrabbitmq/user/UserRepositoryTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -47,6 +50,7 @@ class UserRepositoryTest {
         }
 
         @Test
+        @Order(1)
         @DisplayName("유저 아이디 별로 조회 테스트 케이스")
         void findUserById() {
 
@@ -55,5 +59,14 @@ class UserRepositoryTest {
             assertEquals(mock2.getEmail(), entity.getEmail());
         }
 
+        @Test
+        @Order(2)
+        @DisplayName("유저 아이디 들 별로 조회 테스트 케이스")
+        void findByIdIn() {
+            List<OnlyEmail> entities = userRepository.findUserByIdIn(Collections.singletonList(mock2.getId()), OnlyEmail.class);
+            assertEquals(1, entities.size());
+            OnlyEmail entity = entities.get(0);
+            assertEquals(mock2.getEmail(), entity.getEmail());
+        }
     }
 }
