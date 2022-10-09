00e46ea16a210ba7e7d0137e3e7d406da0e67261
newbalancer <pnw123456@icloud.com>
2022-10-09 20:08:58 +0900

    test: 단일 유저 조회 테스트 케이스

--- email-service/src/test/java/com/example/messagingrabbitmq/user/UserRepositoryTest.java
+++ email-service/src/test/java/com/example/messagingrabbitmq/user/UserRepositoryTest.java
@@ -1,15 +1,14 @@
 package com.example.messagingrabbitmq.user;
 
 import com.example.messagingrabbitmq.util.MockUtil;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -29,5 +28,32 @@ class UserRepositoryTest {
 
         assertNotNull(entity.getId());
         assertEquals(mock.getEmail(), entity.getEmail());
+    }
+
+    @Nested
+    @DisplayName("조회 테스트 케이스")
+    class Select {
+
+        private User mock2;
+
+        @BeforeEach
+        void init() {
+
+            User mock = MockUtil.readJons("json/save.json", User.class);
+
+            mock2 = userRepository.save(mock);
+
+            userRepository.flush();
+        }
+
+        @Test
+        @DisplayName("유저 아이디 별로 조회 테스트 케이스")
+        void findUserById() {
+
+            OnlyEmail entity = userRepository.findUserById(mock2.getId(), OnlyEmail.class).orElseThrow();
+
+            assertEquals(mock2.getEmail(), entity.getEmail());
+        }
+
     }
 }
