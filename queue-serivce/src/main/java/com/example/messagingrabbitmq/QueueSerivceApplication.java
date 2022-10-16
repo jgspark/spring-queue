c13e342671001b483434cdbda8f934f32090f235
newbalancer <pnw123456@icloud.com>
2022-10-16 11:46:46 +0900

    build: aws sqs example code add

---
+++ queue-serivce/src/main/java/com/example/messagingrabbitmq/QueueSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagingrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueSerivceApplication.class, args);
+    }
+
+}
