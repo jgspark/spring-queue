65c5885dc3a33e9304a6e2dc20ce03fe13b32061
newbalancer <pnw123456@icloud.com>
2022-11-19 13:17:34 +0900

    refactor: created logic update

--- outbox/src/main/java/com/example/queue/service/OrderCreatedService.java
+++ outbox/src/main/java/com/example/queue/service/OrderCreatedService.java
@@ -1,12 +1,11 @@
 package com.example.queue.service;
 
+import com.example.queue.domain.Order;
 import com.example.queue.repository.OrderRepository;
 import com.example.queue.service.dto.OrderCreatedDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +14,8 @@ public class OrderCreatedService {
     private final OrderRepository orderRepository;
 
     @Transactional
-    public Optional<OrderCreatedDTO.Response> created(OrderCreatedDTO.Request dto) {
-        OrderCreatedDTO.Response data = new OrderCreatedDTO.Response(orderRepository.save(dto.toEntity()));
-        return Optional.ofNullable(data);
+    public OrderCreatedDTO.Response created(OrderCreatedDTO.Request dto) {
+        Order entity = orderRepository.save(dto.toEntity());
+        return new OrderCreatedDTO.Response(entity);
     }
 }
--- outbox/src/test/java/com/example/queue/service/OrderCreatedServiceTest.java
+++ outbox/src/test/java/com/example/queue/service/OrderCreatedServiceTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.queue.service;
+
+import com.example.queue.domain.Order;
+import com.example.queue.repository.OrderRepository;
+import com.example.queue.service.dto.OrderCreatedDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
 class OrderCreatedServiceTest {
-  
+
+    @InjectMocks
+    private OrderCreatedService orderCreatedService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @BeforeTestClass
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void create_ok() {
+
+        final Long mockId = 1L;
+        final String mockName = "test";
+        Order mock = new Order(mockId, mockName);
+        given(orderRepository.save(any())).willReturn(mock);
+
+        OrderCreatedDTO.Request mockDTO = new OrderCreatedDTO.Request(mockName);
+        OrderCreatedDTO.Response entity = orderCreatedService.created(mockDTO);
+
+        then(orderRepository).should().save(any());
+        assertEquals(mock.getName(), entity.getName());
+    }
 }
