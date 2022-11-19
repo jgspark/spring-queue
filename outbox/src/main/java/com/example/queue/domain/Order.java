e737cdc0ba1860d578ef09a7c86c4fa93f47e9dc
newbalancer <pnw123456@icloud.com>
2022-11-19 12:46:28 +0900

    feat: OrderRepository add

--- outbox/src/main/java/com/example/queue/domain/Order.java
+++ outbox/src/main/java/com/example/queue/domain/Order.java
@@ -1,2 +1,25 @@
-package com.example.queue.domain;public class Order {
+package com.example.queue.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Table
+@Entity(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true)
+    private String name;
+
+    @Builder
+    private Order(String name) {
+        this.name = name;
+    }
 }
--- outbox/src/main/java/com/example/queue/repository/OrderRepository.java
+++ outbox/src/main/java/com/example/queue/repository/OrderRepository.java
@@ -1,2 +1,9 @@
-package com.example.queue.repository;public interface OrderRepository {
+package com.example.queue.repository;
+
+import com.example.queue.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
