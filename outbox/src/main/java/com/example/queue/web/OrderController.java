45aeced1fbd6aba0cad35be243856dfbfac22b66
newbalancer <pnw123456@icloud.com>
2022-11-19 13:13:41 +0900

    fix: order created api change to Object

--- outbox/src/main/java/com/example/queue/web/OrderController.java
+++ outbox/src/main/java/com/example/queue/web/OrderController.java
@@ -3,13 +3,10 @@ package com.example.queue.web;
 import com.example.queue.service.OrderCreatedService;
 import com.example.queue.service.dto.OrderCreatedDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("order")
@@ -19,11 +16,7 @@ public class OrderController {
     private final OrderCreatedService orderCreatedService;
 
     @PostMapping
-    public ResponseEntity<OrderCreatedDTO.Response> created(@RequestBody OrderCreatedDTO.Request dto) {
-        Optional<OrderCreatedDTO.Response> optional = orderCreatedService.created(dto);
-        if (optional.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(optional.get());
+    public OrderCreatedDTO.Response created(@RequestBody OrderCreatedDTO.Request dto) {
+        return orderCreatedService.created(dto);
     }
 }
